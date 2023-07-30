4dcf9acd93922f87b7474deab952ff31ba6170f9
babujal <dercamar1@gmail.com>
2023-07-29 21:49:45 -0400

    need to change dl elements by table and add movies cards

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ window.onload = (e) => {
     const tagleLoading = () => {
         const barDiv = document.querySelector('.barDiv')
         const loading = document.createElement('div')
-        loading.className = 'loadingBar'
+        loading.id = 'loadingBar'
         barDiv.appendChild(loading)
-        const collectingText = document.querySelector('.loadingBar')
+        const collectingText = document.querySelector('#loadingBar')
         collectingText.innerText = 'colecting data'
     } 
 
@@ -20,40 +20,31 @@ window.onload = (e) => {
                 return response.json()
             })
             .then((data) => {
+                console.log(data)
+                $('loadingBar').remove()
                 // All I want to do with the data goes down here
                 const userInput = document.querySelector('#title')
                 let placeHolderVal = userInput.value
                 // console.log(placeHolderVal)
-                const getTitles = () => {
-                    let titles = []
                     for (let i = 0; i < data.results.length; i++) {
                         const arrays = data.results[i]
                         let titleInArray = arrays.title.toLowerCase()
-                        titles.push(titleInArray) // is been used just to know the titles
                         if (titleInArray.includes(placeHolderVal.toLowerCase())) {
                             //Down here asign values to DOM using arrays.
                             // console.log(arrays)
                             // console.log(arrays.director)
                             document.querySelector('.title').innerText = arrays.title
                             document.querySelector('.episode').innerText = arrays.episode_id
-                            document.querySelector('.opening').innerText = arrays.opening_crawl
+                            const openingWrap = document.querySelector('#openingWrap')
+                            const opening = document.createElement('dl')
+                            opening.className = 'opening'
+                            openingWrap.appendChild(opening)
+                            opening.innerText = arrays.opening_crawl
                             document.querySelector('.director').innerText = arrays.director
                             document.querySelector('.producer').innerText = arrays.producer
-                            console.log(arrays)
                         }
                     }
-                    return titles
-                }
-                getTitles()
-                // console.log(getTitles())
-                // console.log(placeHolderVal)
-                // console.log(getTitles())
-                // console.log(data.results)
-                // console.log(data.results[1])
-                // if (titles.includes(placeHolderVal)) {
-                //     //assing place holder to the title dot notation and use it as a key to access values in that object
-                //     //the index of titles[] is the indexOF the results array of objects
-                // }
+                
             })
             .catch((error) => {
                 console.error("Error fetching data:", error)
@@ -66,3 +57,5 @@ window.onload = (e) => {
 
     })
 }
+
+document.querySelector('.opening').innerText = arrays.opening_crawl
