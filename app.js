35884b3bb191a23390ceaa2af4a8f0ecacda19e0
babujal <dercamar1@gmail.com>
2023-07-31 22:33:45 -0400

    star wars inquiry site

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const $ = (id) => {return document.getElementById(id)}
 const getElByClass = (elClass) => {return document.querySelector(elClass)}
 window.onload = (e) => { 
-    const tagleLoading = () => {
+    const taggleLoading = () => {
         const barDiv = document.querySelector('.barDiv')
         const loading = document.createElement('div')
         loading.id = 'loadingBar'
@@ -29,7 +29,7 @@ window.onload = (e) => {
                     for (let i = 0; i < data.results.length; i++) {
                         const arrays = data.results[i]
                         let titleInArray = arrays.title.toLowerCase()
-                        if (titleInArray.includes(placeHolderVal.toLowerCase())) {
+                        if ((titleInArray.includes(placeHolderVal.toLowerCase())) || (titleInArray.includes(getElByClass('.imgWrap').innerText))) {
                             //Down here asign values to DOM using arrays.
                             document.querySelector('.title').innerText = arrays.title
                             document.querySelector('.episode').innerText = arrays.episode_id
@@ -49,14 +49,55 @@ window.onload = (e) => {
         });
     }
     document.querySelector('#submit').addEventListener('click', (e) => {
-        
         e.preventDefault()
-        tagleLoading()
+        //bellow call the function for loading bar
+        taggleLoading()
+        //bellow calling the fetch data function
         fetchData()
         //bellow removes opening text when a new search is done so isn't x2
         getElByClass('.opening').remove()
+    })
 
+    //Assign a value to the place holder for the event listener in the cards
+    const asignValue = (param) => {
+        const img1 = getElByClass(param)
+        const img1Text = img1.innerText
+        const userInput1 = document.querySelector('#title')
+        return userInput1.value = img1Text
+        taggleLoading()
+        fetchData()
+        getElByClass('.opening').remove()
+    }
+    //To call function outside of event listener
+    const callFunc = () => {
+        taggleLoading()
+        fetchData()
+        getElByClass('.opening').remove()
+    }
+    //Event listener for the cards
+    document.querySelector('.imgWrap1').addEventListener('click', () => {
+        asignValue('.imgWrap1')
+        callFunc()
+    })
+    document.querySelector('.imgWrap2').addEventListener('click', () => {
+        asignValue('.imgWrap2')
+        callFunc()
+    })
+    document.querySelector('.imgWrap3').addEventListener('click', () => {
+        asignValue('.imgWrap3')
+        callFunc()
+    })
+    document.querySelector('.imgWrap4').addEventListener('click', () => {
+        asignValue('.imgWrap4')
+        callFunc()
+    })
+    document.querySelector('.imgWrap5').addEventListener('click', () => {
+        asignValue('.imgWrap5')
+        callFunc()
+    })
+    document.querySelector('.imgWrap6').addEventListener('click', () => {
+        asignValue('.imgWrap6')
+        callFunc()
     })
 }
 
-document.querySelector('.opening').innerText = arrays.opening_crawl
